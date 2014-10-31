6cabcc7295686d9c241cd286473c8f00518ec2f7
Andy Berry <andyberry88@gmail.com>
2014-10-31 11:55:33 +0000

    adding min and max exec duration and file counts

--- blades/avgbundlesetduration/src/AverageBundlesetDurationViewModel.js
+++ blades/avgbundlesetduration/src/AverageBundlesetDurationViewModel.js
@@ -4,6 +4,21 @@ var Keen = require( 'keen-js' );
 
 function AverageBundlesetDurationViewModel() {
 
+	var minBundlesetExecTime = new Keen.Query("minimum", {
+		eventCollection: "bundlesets",
+		targetProperty: "execution_duration" // get sub-property - cool that you can do this!
+	});
+	var minBundlesetExecTimeRequest = window.KEEN_CLIENT.run(minBundlesetExecTime, function(response){
+	// Pass in raw data, or reference to "this" (request instance)
+	var myChart = new Keen.Visualization(this, document.getElementById("min-bundleset-time"), {
+		chartType: "metric",
+		title: "min milliseconds",
+		colors: ["#49c5b1"],
+			width: 'auto'
+		});
+	});
+
+
 	var averageBundlesetExecTime = new Keen.Query("average", {
 		eventCollection: "bundlesets",
 		targetProperty: "execution_duration" // get sub-property - cool that you can do this!
@@ -12,7 +27,22 @@ function AverageBundlesetDurationViewModel() {
 	// Pass in raw data, or reference to "this" (request instance)
 	var myChart = new Keen.Visualization(this, document.getElementById("avg-bundleset-time"), {
 		chartType: "metric",
-		title: "milliseconds",
+		title: "average milliseconds",
+		colors: ["#49c5b1"],
+			width: 'auto'
+		});
+	});
+
+
+	var maxBundlesetExecTime = new Keen.Query("maximum", {
+		eventCollection: "bundlesets",
+		targetProperty: "execution_duration" // get sub-property - cool that you can do this!
+	});
+	var maxBundlesetExecTimeRequest = window.KEEN_CLIENT.run(maxBundlesetExecTime, function(response){
+	// Pass in raw data, or reference to "this" (request instance)
+	var myChart = new Keen.Visualization(this, document.getElementById("max-bundleset-time"), {
+		chartType: "metric",
+		title: "max milliseconds",
 		colors: ["#49c5b1"],
 			width: 'auto'
 		});
@@ -20,7 +50,9 @@ function AverageBundlesetDurationViewModel() {
 
 	// TODO: update to be triggered upon Firebase update
 	setInterval( function() {
+		minBundlesetExecTimeRequest.refresh();
 		averageBundlesetExecTimeRequest.refresh();
+		maxBundlesetExecTimeRequest.refresh();
 	}, 10000 );
 
 }
--- blades/avgfilesinapp/src/AvgfilesinappViewModel.js
+++ blades/avgfilesinapp/src/AvgfilesinappViewModel.js
@@ -4,25 +4,59 @@ var Keen = require( 'keen-js' );
 
 function AverageFilesInApp() {
 
-	var averageAppFileCount = new Keen.Query("average", {
+	var minAppFileCount = new Keen.Query("minimum", {
   		eventCollection: "bundlesets",
   		targetProperty: "file_count.total_count" // get sub-property - cool that you can do this!
 	});
 
 	// TODO: separate request and response from UI update (Visualization)
-	var averageAppFileRequest = window.KEEN_CLIENT.run(averageAppFileCount, function(response){
+	var minAppFileRequest = window.KEEN_CLIENT.run(minAppFileCount, function(response){
 	  // Pass in raw data, or reference to "this" (request instance)
-	  var myChart = new Keen.Visualization(this, document.getElementById("avg-files"), {
+	  var myChart = new Keen.Visualization(this, document.getElementById("min-files"), {
 		  chartType: "metric",
-		  title: "Files",
+		  title: "Min Files",
 		  colors: ["#49c5b1"],
+			width: 'auto'
+		});
+	});
+
+	var averageAppFileCount = new Keen.Query("average", {
+		eventCollection: "bundlesets",
+		targetProperty: "file_count.total_count" // get sub-property - cool that you can do this!
+	});
+
+	// TODO: separate request and response from UI update (Visualization)
+	var averageAppFileRequest = window.KEEN_CLIENT.run(averageAppFileCount, function(response){
+	// Pass in raw data, or reference to "this" (request instance)
+	var myChart = new Keen.Visualization(this, document.getElementById("avg-files"), {
+		chartType: "metric",
+		title: "Avg Files",
+		colors: ["#49c5b1"],
+			width: 'auto'
+		});
+	});
+
+	var maxAppFileCount = new Keen.Query("maximum", {
+		eventCollection: "bundlesets",
+		targetProperty: "file_count.total_count" // get sub-property - cool that you can do this!
+	});
+
+	// TODO: separate request and response from UI update (Visualization)
+	var maxAppFileRequest = window.KEEN_CLIENT.run(maxAppFileCount, function(response){
+	// Pass in raw data, or reference to "this" (request instance)
+	var myChart = new Keen.Visualization(this, document.getElementById("max-files"), {
+		chartType: "metric",
+		title: "Max Files",
+		colors: ["#49c5b1"],
 			width: 'auto'
 		});
 	});
 
 	// TODO: update to be triggered upon Firebase update
 	setInterval( function() {
+		minAppFileRequest.refresh();
 		averageAppFileRequest.refresh();
+		maxAppFileRequest.refresh();
 	}, 10000 );
 
 }
