0dbaed23c50af3e41c7f619e8e1081178a66faa6
Andy Berry <andyberry88@gmail.com>
2014-10-31 10:21:49 +0000

    adding OS info widget

--- blades/osinfo/src/OsinfoViewModel.js
+++ blades/osinfo/src/OsinfoViewModel.js
@@ -5,20 +5,39 @@ var i18n = require( 'br/I18n' );
 var ServiceRegistry = require( 'br/ServiceRegistry' );
 
 function OsinfoViewModel() {
-	this.eventHub = ServiceRegistry.getService( 'br.event-hub' );
-	this.welcomeMessage = ko.observable( 'Welcome to your new Blade.' );
-	this.buttonClickMessage = ko.observable( i18n( 'dashboard.osinfo.button.click.message' ) );
-	this.logWelcome();
 }
 
-OsinfoViewModel.prototype.buttonClicked = function() {
-	console.log( 'button clicked' );
-	var channel = this.eventHub.channel('osinfo-channel');
-	channel.trigger( 'hello-event', { some: 'Hello World!' } );
-};
 
-OsinfoViewModel.prototype.logWelcome = function() {
-	console.log(  this.welcomeMessage() );
+OsinfoViewModel.prototype.init = function() {
+	// TODO: Move client into a service
+	var client = new Keen({
+	projectId: "5452adc733e406748303ecb4",
+	readKey: "0e4cf7ec70352aa3f109d88f9fdf9fe67e91f78a0a01d4f3d132c53c03730cee2d3a9a5a4631b28574f467fea78b0fdb9586ca723cb006eb94b8e8a1bc50abc19e0d90e939df51d1f81b85311c3e590d22e9a4e84d2f3efcacc642ff8dc70ed284301399c9ef7eeb78ce04a8799e8c26"
+	});
+
+	var osName = new Keen.Query("count", {
+		eventCollection: "installs",
+		groupBy: "os_name"
+	});
+	client.draw(osName,
+		document.getElementById("os-info-name"), {
+		chartType: "piechart",
+		title: "OS Name"
+	});
+
+	var javaVersion = new Keen.Query("count", {
+		eventCollection: "installs",
+		groupBy: "java_version"
+	});
+	client.draw(javaVersion,
+		document.getElementById("os-info-java-version"), {
+		chartType: "piechart",
+		title: "Java Version"
+	});
+
+	setInterval( function() {
+		request.refresh();
+	}, 10000 );
 }
 
 module.exports = OsinfoViewModel;
