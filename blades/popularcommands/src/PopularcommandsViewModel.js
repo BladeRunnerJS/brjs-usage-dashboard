d6ef17c074e7ef9f769feefddeeaaa46a9d4319c
Andy Berry <andyberry88@gmail.com>
2014-10-31 10:55:46 +0000

    adding popular commands graph

--- blades/popularcommands/src/PopularcommandsViewModel.js
+++ blades/popularcommands/src/PopularcommandsViewModel.js
@@ -5,20 +5,23 @@ var i18n = require( 'br/I18n' );
 var ServiceRegistry = require( 'br/ServiceRegistry' );
 
 function PopularcommandsViewModel() {
-	this.eventHub = ServiceRegistry.getService( 'br.event-hub' );
-	this.welcomeMessage = ko.observable( 'Welcome to your new Blade.' );
-	this.buttonClickMessage = ko.observable( i18n( 'dashboard.popularcommands.button.click.message' ) );
-	this.logWelcome();
 }
 
-PopularcommandsViewModel.prototype.buttonClicked = function() {
-	console.log( 'button clicked' );
-	var channel = this.eventHub.channel('popularcommands-channel');
-	channel.trigger( 'hello-event', { some: 'Hello World!' } );
-};
+PopularcommandsViewModel.prototype.init = function() {
 
-PopularcommandsViewModel.prototype.logWelcome = function() {
-	console.log(  this.welcomeMessage() );
+	var popularCommands = new Keen.Query("count", {
+	    eventCollection: "commands",
+	    timeframe: "this_7_days",
+	    interval: "daily",
+	    groupBy: "command_name"
+  });
+
+
+	window.KEEN_CLIENT.draw(popularCommands,
+		document.getElementById("popular-commands"), {
+		title: "Popular Commands",
+	});
+
 }
 
 module.exports = PopularcommandsViewModel;
